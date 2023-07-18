890cb65299b89d9b23241b01455539e441071b97
Joe Previte <jjprevite@gmail.com>
2023-07-18 01:50:17 +0000

    first commit

---
+++ app/components/SignOut.tsx
@@ -0,0 +1,11 @@
+"use client"
+import { useClerk } from "@clerk/clerk-react";
+
+const SignOutButton = () => {
+  const { signOut } = useClerk();
+  return (
+    <button onClick={() => signOut()} >
+      Sign out
+    </button>
+  );
+};
